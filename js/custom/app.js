6e64164027f02278849a0fcd7f5fcc09b645f3f5
Tim J. Peters <tim.peters@stud.h-da.de>
2014-12-01 16:32:05 +0100

    Issue #2 : Client server communication functions

--- js/custom/app.js
+++ js/custom/app.js
@@ -6,19 +6,50 @@ var app = angular.module('radar', ['geolocation']);
 // Angular Database Requests
 // Holt die Clubs (samt Eigenschaften) aus einem json und speichert sie für angular lesbar 
 app.controller('AppCtrl', function($scope, $http, geolocation) {
-    $scope.values = {};
     
+    /* Initiale clientInfos
+     * FIXME: userId muss noch ermittelt & hinzugefügrt werden */ 
+    var clientInfos = {
+        "_userId": "",
+        "position": [],
+        "zoomLvl": 0
+    }
+
     geolocation.getLocation().then(function(data){
-      $scope.coords = {lat:data.coords.latitude, long:data.coords.longitude};
+      clientInfos.position = [data.coords.latitude, data.coords.longitude];
+      $scope.getData(clientInfos); // erst sobald die position da ist, werden die Club-Circles erzeugt
     });
 
     // Erste initialisierung und funktion um sich die aktuellen Daten vom Server zu holen
-    $scope.getData = function() {
-        $http.get('./db/clubs.json').then(function(clubsResponse) {
-            $scope.result = clubsResponse.data;
-        });
+    $scope.getData = function(clientInfos) {
+        if(clientInfos != null)
+            $http.post('./db/clubs.json', clientInfos).then(function(radarResponse) {
+                console.log(JSON.stringify(clientInfos));
+                $scope.result = radarResponse.data;
+            });
     }
-    $scope.getData(true);
+
+    /* Schränkt das Radar auf einen kleineren Bereich/weniger Clubs ein 
+     * solange das Minimum (0) noch nicht erreicht ist
+     * FIXME: Animation hinzufügen */
+    $scope.zoomIn = function() {
+        if(clientInfos.zoomLvl > 0)
+        {
+            clientInfos.zoomLvl--;
+            $scope.getData(clientInfos);
+        }
+    }
+
+    /* Erweitert das Radar um weitere Clubs 
+     * solange das Maximum noch nicht erreicht ist
+     * FIXME: Animation hinzufügen */
+    $scope.zoomOut = function() {
+        if(!$scope.result.isMax) 
+        {
+            clientInfos.zoomLvl++;
+            $scope.getData(clientInfos);
+        }
+    }
 });
 
 if (window.DeviceOrientationEvent) {

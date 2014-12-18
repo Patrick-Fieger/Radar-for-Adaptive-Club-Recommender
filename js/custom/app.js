c54d69894ee3ab5183c2eb75fdaa1def9837f15b
Patrick Fieger <patrickfieger90@gmail.com>
2014-12-18 16:26:35 +0100

    Revert 3f8999f..95b30bc
    This rolls back to commit 3f8999f0c24e96d7dacb46dd337f9800aff43d71.

--- js/custom/app.js
+++ js/custom/app.js
@@ -1,44 +1,52 @@
 // Speicher Values in Array bei click
 'use strict';
-var app = angular.module('radar', ['geolocation', 'ngAnimate']);
+var app = angular.module('radar', ['geolocation']);
 var isMobil = false;
+
 // Angular Database Requests
 // Holt die Clubs (samt Eigenschaften) aus einem json und speichert sie für angular lesbar 
 app.controller('AppCtrl', function($scope, $http, geolocation) {
+    
     /* Initiale clientInfos
-     * FIXME: userId muss noch ermittelt & hinzugefügrt werden */
+     * FIXME: userId muss noch ermittelt & hinzugefügrt werden */ 
     $scope.clientInfos = {
         "userId": "",
         "position": [],
         "scale": 0
     }
-    geolocation.getLocation().then(function(data) {
-        $scope.clientInfos.position = [data.coords.latitude, data.coords.longitude];
-        $scope.getData($scope.clientInfos); // erst sobald die position da ist, werden die Club-Circles erzeugt
+
+    geolocation.getLocation().then(function(data){
+      $scope.clientInfos.position = [data.coords.latitude, data.coords.longitude];
+      $scope.getData($scope.clientInfos); // erst sobald die position da ist, werden die Club-Circles erzeugt
     });
+
     // Erste initialisierung und funktion um sich die aktuellen Daten vom Server zu holen
     $scope.getData = function(clientInfos) {
-        if (clientInfos != null)
-        //$http.post('./db/clubs.json', clientInfos).then(function(radarResponse) {
-            $http.post('./db/clubs_' + $scope.clientInfos.scale + '.json', $scope.clientInfos).then(function(radarResponse) { // Behelfslösung bis der Server steht
+        if(clientInfos != null)
+            //$http.post('./db/clubs.json', clientInfos).then(function(radarResponse) {
+            $http.post('./db/clubs_'+$scope.clientInfos.scale+'.json', $scope.clientInfos).then(function(radarResponse) { // Behelfslösung bis der Server steht
                 debug(JSON.stringify($scope.clientInfos));
                 $scope.result = radarResponse.data;
             });
     }
+
     /* Schränkt das Radar auf einen kleineren Bereich/weniger Clubs ein 
      * solange das Minimum (0) noch nicht erreicht ist
      * FIXME: Animation hinzufügen */
     $scope.zoomIn = function() {
-        if ($scope.clientInfos.scale > 0) {
+        if($scope.clientInfos.scale > 0)
+        {
             $scope.clientInfos.scale--;
             $scope.getData($scope.clientInfos);
         }
     }
+
     /* Erweitert das Radar um weitere Clubs 
      * solange das Maximum noch nicht erreicht ist
      * FIXME: Animation hinzufügen */
     $scope.zoomOut = function() {
-        if (!$scope.result.isMax) {
+        if(!$scope.result.isMax) 
+        {
             $scope.clientInfos.scale++;
             $scope.getData($scope.clientInfos);
         }
@@ -46,16 +54,19 @@ app.controller('AppCtrl', function($scope, $http, geolocation) {
 });
 
 function debug(content) {
-    if (isMobil != null && isMobil == true) alert(content);
-    else console.log(content);
+    if(isMobil != null && isMobil == true)
+        alert(content);
+    else
+        console.log(content);
 }
+
 if (window.DeviceOrientationEvent) {
-    window.addEventListener("deviceorientation", function(e) {
+    window.addEventListener("deviceorientation", function (e) {
         rotate(e.alpha);
-        if (e.alpha != 0) isMobil = true;
+        if(e.alpha != 0) isMobil = true;
     }, false);
 }
 
-function rotate(deg) {
-    $('#radar_1').css('transform', 'rotate(' + deg + 'deg)');
+function rotate(deg){
+	$('#radar_1').css('transform', 'rotate(' + deg + 'deg)');
 }

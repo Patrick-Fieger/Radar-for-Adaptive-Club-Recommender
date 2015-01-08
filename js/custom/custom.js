9b8c117497c1d7bd22448465242116e7351f947f
tim-peters <tim_peters@gmx.de>
2015-01-09 00:25:47 +0100

    Merge of Volker's design fork & formatting

---
+++ js/custom/custom.js
@@ -0,0 +1,29 @@
+$(function() {
+    $( ".slider" ).slider({
+      animate: true,
+      range: "min",
+      value: 0,
+      min: 10,
+      max: 100,
+      step: 10,
+
+      //this gets a live reading of the value and prints it on the page
+      slide: function(event, ui) {
+        $("#slider-result").html(ui.value);
+      },
+
+      //this updates the hidden form field so we can submit the data using a form
+      change: function(event, ui) {
+        $('#hidden').attr('value', ui.value);
+      }
+    });
+
+    $(".clubCircle").click(function() {
+      $(".tooltip").toggleClass("active"); 
+    });
+
+    $(".close").click(function() {
+      $(".tooltip .active").removeClass("active"); 
+    });
+
+});
